c8cba53c23486302e4bfb5aecef8d08eefe51687
Dinh Tien <tiendv@ahamove.com>
2021-12-01 11:26:23 +0700

    Change pod

---
+++ ToolDuong/Cells/ResultsCell.swift
@@ -0,0 +1,23 @@
+//
+//  ResultsCell.swift
+//  ToolDuong
+//
+//  Created by Tien Dinh on 27/11/2021.
+//
+
+import UIKit
+
+class ResultsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ ToolDuong/UserdefaultHelper.swift
@@ -0,0 +1,8 @@
+//
+//  UserdefaultHelper.swift
+//  ToolDuong
+//
+//  Created by Tien Dinh on 30/11/2021.
+//
+
+import Foundation
